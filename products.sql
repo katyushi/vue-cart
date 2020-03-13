92082a1340f924d188c445c9f86dcfe0ee4c29af
hugo lazzari <hugo.luiz@outlook.com>
2020-03-13 17:55:25 -0300

    sql script with some products

---
+++ products.sql
@@ -0,0 +1,34 @@
+INSERT INTO `products`(
+    `title`,
+    `picture`,
+    `description`,
+    `price`,
+    `qtd`,
+    `created_at`,
+    `updated_at`
+)
+VALUES(
+    'For Loops™',
+    'https://pbs.twimg.com/media/EGSGpCBWsAAY3lj.jpg',
+    'breakfast and arrays start with 0',
+    '9.99',
+    '500',
+    '2020-03-12 17:55:39',
+    '2020-03-12 19:05:47'
+),(
+    'Breggsit™',
+    'https://66.media.tumblr.com/6acdacfe52a4ef04f5a2853271693a14/b41ed438871f688d-c0/s640x960/735db68ae24a0be8babb0f28de753368b0e3b6dd.jpg',
+    'Breggsit™: \"the anticipation is the best part\"™',
+    '5.99',
+    '258',
+    '2020-03-12 19:05:23',
+    '2020-03-12 19:05:23'
+),(
+    'Microsoft Edgy',
+    'https://pm1.narvii.com/7068/1b5fcf50087d344bdf5258fe33de2224b6f6e217r1-800-455v2_hq.jpg',
+    'the edgiest web browser',
+    '-5.99',
+    '9999',
+    '2020-03-12 19:08:20',
+    '2020-03-12 19:08:20'
+)
